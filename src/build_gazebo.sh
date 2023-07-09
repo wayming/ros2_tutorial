d5c3c0b45799d12312059934513f67dc7d922f6b
wayming.z <wayming.z@gmail.com>
2023-07-09 14:35:55 +1000

    start

--- src/build_gazebo.sh
+++ src/build_gazebo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm -rf src/aerostack2
-git clone https://github.com/aerostack2/aerostack2.git src/aerostack2
-git clone https://github.com/aerostack2/project_gazebo src/aerostack2/projects/gazebo
+# rm -rf src/aerostack2
+# git clone https://github.com/aerostack2/aerostack2.git src/aerostack2
+# git clone https://github.com/aerostack2/project_gazebo src/aerostack2/projects/gazebo
 
 rm -rf $HOME/.ros2.bash
 echo "source /opt/ros/humble/setup.bash" >> $HOME/.ros2.bash
 echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> $HOME/.ros2.bash
-echo "export TURTLEBOT3_MODEL=waffle" >> $HOME/.ros2.bash
-echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> $HOME/.ros2.bash
+#echo "export TURTLEBOT3_MODEL=waffle" >> $HOME/.ros2.bash
+#echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> $HOME/.ros2.bash
 echo "export AEROSTACK2_PATH=$HOME/src/aerostack2" >> $HOME/.ros2.bash
 echo "source $HOME/src/aerostack2/as2_cli/setup_env.bash" >> $HOME/.ros2.bash
 echo "source $HOME/.ros2.bash" >> $HOME/.bashrc
